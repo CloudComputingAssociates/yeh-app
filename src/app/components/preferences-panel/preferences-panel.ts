44415402a054a6dd9e168276d3476502b38eabdd
martymazurik <your-email@example.com>
2025-12-08 14:09:59 -0800

    tighten radio buttons

--- src/app/components/preferences-panel/preferences-panel.ts
+++ src/app/components/preferences-panel/preferences-panel.ts
@@ -1,14 +1,12 @@
 // src/app/components/preferences-panel/preferences-panel.ts
-import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabService } from '../../services/tab.service';
-import { FoodsComponent, SelectedFoodEvent, AddFoodEvent } from '../foods/foods';
-import { SelectedFoodsComponent, RemoveFoodEvent } from '../selected-foods/selected-foods';
-import { Food } from '../../models/food.model';
+import { FoodsComponent, SelectedFoodEvent } from '../foods/foods';
 
 @Component({
   selector: 'app-preferences-panel',
-  imports: [CommonModule, FoodsComponent, SelectedFoodsComponent],
+  imports: [CommonModule, FoodsComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div class="panel-container">
@@ -19,25 +17,12 @@ import { Food } from '../../models/food.model';
 
       <div class="panel-content">
         <div class="foods-section">
-          <div class="foods-panel">
-            <div class="picker-column">
-              <h3 class="column-title">FOOD PICKER</h3>
-              <app-foods
-                [mode]="'search'"
-                [showAiButton]="false"
-                [showPreferenceIcons]="true"
-                [showFilterRadios]="true"
-                (selectedFood)="onFoodSelected($event)"
-                (addFood)="onAddFood($event)" />
-            </div>
-
-            <div class="selected-column">
-              <h3 class="column-title">SELECTED FOODS</h3>
-              <app-selected-foods
-                [foods]="selectedFoods()"
-                (removeFood)="onRemoveFood($event)" />
-            </div>
-          </div>
+          <app-foods
+            [mode]="'search'"
+            [showAiButton]="false"
+            [showPreferenceIcons]="true"
+            [showFilterRadios]="true"
+            (selectedFood)="onFoodSelected($event)" />
         </div>
       </div>
     </div>
@@ -47,39 +32,11 @@ import { Food } from '../../models/food.model';
 export class PreferencesPanelComponent {
   private tabService = inject(TabService);
 
-  // State for foods selection
-  selectedFoods = signal<Food[]>([]);
-
   close(): void {
     this.tabService.closeTab('preferences');
   }
 
   onFoodSelected(event: SelectedFoodEvent): void {
     console.log('Food selected in Preferences:', event.description);
-  }
-
-  onAddFood(event: AddFoodEvent): void {
-    this.addToSelectedFoods(event.food);
-  }
-
-  onRemoveFood(event: RemoveFoodEvent): void {
-    this.removeFromSelectedFoods(event.food);
-  }
-
-  private addToSelectedFoods(food: Food): void {
-    const currentFoods = this.selectedFoods();
-    const alreadyExists = currentFoods.some(f => f.id === food.id);
-
-    if (!alreadyExists) {
-      this.selectedFoods.set([...currentFoods, food]);
-      console.log('Added food to preferences list:', food.description);
-    }
-  }
-
-  private removeFromSelectedFoods(food: Food): void {
-    const currentFoods = this.selectedFoods();
-    const updatedFoods = currentFoods.filter(f => f.id !== food.id);
-    this.selectedFoods.set(updatedFoods);
-    console.log('Removed food from preferences list:', food.description);
   }
 }
